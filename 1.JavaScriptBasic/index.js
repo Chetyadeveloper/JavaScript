20f62819a6b1e60025b02366994e4663638f1ed5
Chetyadeveloper <chetankamble3520@gmail.com>
2023-02-21 16:05:07 +0530

    firstchange

---
+++ 1.JavaScriptBasic/index.js
@@ -0,0 +1,21 @@
+/* prininting normal message:---*/
+console.log('this is log Message');
+
+/* priniting Information Message */
+console.info('this is information messgae');
+
+/*printing error  message*/
+console.error('this is error  message');
+
+/*prinitning warning message*/
+console.warn('this is warning message');
+
+/*prininting table data */
+
+let student ={
+    name : 'chetya',
+    age  :29 ,
+    city : 'hyd'
+
+};
+console.table(student);
