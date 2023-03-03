1210a252624956d2bf579e9538d08f13d8d7b543
Chetyadeveloper <chetankamble3520@gmail.com>
2023-03-03 14:11:06 +0530

    change

--- 2.JavaScriptVariable/index.js
+++ 2.JavaScriptVariable/index.js
@@ -18,4 +18,41 @@ console.info('the sum is' + a + ' , ' + b + 'is :'+sum); //ES5
 console.log(`The sum is ${a} , ${b} is : ${sum}`); //ES6
 
 
+let name = 'chetan' ;
+name = 'kamble';
+console.log(name); // kamble (data override)
+
+
 //Rules of Declaring Variable
+/*
+
+1. All variable are case sensitive
+    Example :--
+      let name='Naveen';
+      console.log(Name); 
+
+2. All variable are in camelCase
+     Example :--
+      let myEmpName = 'John';
+      console.log(myEmpName);
+
+3. max allow char 15
+
+4. chars allowed are  a-z,A-Z,$,_,0-9
+    Example:--
+      let number  = 10 ;  // valid
+      let $number = 10 ;  // valid
+      let _number = 10 ;  // valid
+      let 1number = 10 ;  // not valid(bcz variable not start with number )
+      let #number ='John' ;  // not valid
+         
+5. variable cannot be re-declared
+    Example:--
+        let name = 'Chetan';
+        let name = 'kamble';
+        console.log(name); // error variable cannot be re decleared.
+                             Uncaught SyntaxError: Identifier 'name' has already been declared.
+
+
+*/
+
