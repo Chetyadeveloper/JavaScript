cbbfd6cd28ed4b95c6747de178784a5b5b3161da
Chetyadeveloper <chetankamble3520@gmail.com>
2023-03-05 16:03:06 +0530

    c

--- 6.JavaScriptLooping/index.js
+++ 6.JavaScriptLooping/index.js
@@ -1,3 +1,63 @@
 //for loop
 //while loop
 // do-while loop
+
+
+//Examples :---
+
+//for loop :--
+    
+    /* Syntax:  for(initization;condition;increment/decerement){
+                     // block of statement ;                  
+                    }     
+    */
+
+           //example : 1 .  
+                            let result = '';
+                             for (let i = 0; i < 5; i++) {
+                                    result +=i;
+                                
+                             } 
+                             console.log(`${result}`);        
+
+            //example : 2 .  
+                            let resultt = '';
+                            let i=0;
+                            for (; i < 5; i++) {
+                                resultt +=i;
+                            
+                            } 
+                            console.log(`${resultt}`);  
+                            
+            //example : 3 .  
+                        
+                            let a = 0;
+                            for (; a < 5; ) {
+                                console.log(a);
+                                a++;
+                            
+                            } 
+
+             //example : 4.  
+                             let c = 0;
+                             for (; ; c++) {
+                                if (c==3) {
+                                    break;
+                                }
+                                console.log(c);
+                            }               
+                                 
+             // example 5 :
+                             console.log(`cities`);
+                             let r='';
+                             let cities = ['Hyd','Che','Ban','Ngp'];
+                             for (let index = 0; index < cities.length; index++) {
+                                   r+=`${cities[index]},`;     
+                                 
+                                
+                             }
+                             console.log(r);
+
+                            
+
+                             
--- 8.JavaScriptArray/index.js
+++ 8.JavaScriptArray/index.js
@@ -1,3 +1,83 @@
+/* Array : Array is nothing but collection of data item.
+           Array can be divided into two ways.
+           1. array using literals
+           2. array using constructor
+           
+     
+*/  
+    //  1. Array using Literals :---
+      
+                console.log('=== Array Literals');   
+                let geeks = [];
+                // adding element to array
+                geeks[0] = 'apple';
+                geeks[1] = 'banana';
+                geeks[2] = 'bitroot';
+                geeks[3] = 'watermelon';
+                geeks[4] = 'graps';
+                console.log(geeks);
+
+                //accessing element from Array
+                console.log('==Accessing Element from Element');
+                console.log(geeks[0]);  // apple
+                console.log(geeks[1]);  // banana
+                console.log(geeks[2]);  // bitroot
+                console.log(geeks[3]);  // watermelon
+                console.log(geeks[4]);  // graps
+
+                // modification of element from Array
+                 console.log('==Modification of element from Array==');
+                 geeks[0] = 'ramfruit';
+                 console.log(geeks);
+
+                 // delete of element from Array
+                 console.log('==Delete of element from Array==');
+                 delete geeks[0];
+                 console.log(geeks);
+
+     // 2. Array using Constructor :---
+
+                console.log('==Array Constructor==');
+                let honey=new Array();   // empty Array
+                 honey[0] = 'HTML';
+                 honey[1] = 'CSS';
+                 honey[2] = 'Javascript';
+                 console.log(honey);
+
+
+                 //Accessing element from Array constructor
+                 console.log('===Accessing of element from Array Constructor');
+                 console.log(honey[0]); //HTML
+                 console.log(honey[1]); // CSS
+                 console.log(honey[2]); // JavaScript
+
+
+            // Iteration of Array 
+                 console.log('===Iteration of Array');
+                 let gee = ['Rahul','Ram',56,'Jay'];
+                 for(let i=0;i<4;i++){
+                       console.log(`index of ${i} is : ${gee[i]} `);
+                 }
+
+            // forEach 
+                console.log('==forEach==');
+                gee.forEach(function(value,index){
+                    console.log(`the value is :${value} of  ${index}`);
+                });                   
+           
+          // for of
+             console.log('==for of ==');
+             for (let x of gee) {
+                 console.log(x);
+             }
+             
+
+
+
+
+
+
+
 //Array Method
 /*
    1.shift() --> remove the first element from the array.
