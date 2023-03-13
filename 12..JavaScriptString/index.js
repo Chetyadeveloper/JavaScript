b5a2acfa73c2b747a99978e6c2a68286bb05a310
Chetyadeveloper <chetankamble3520@gmail.com>
2023-03-13 16:32:52 +0530

    string

---
+++ 12..JavaScriptString/index.js
@@ -0,0 +1,28 @@
+// Javascript String
+
+let msg = 'Good Morning';
+
+//  length
+
+console.log(msg.length); //12
+
+//LowerCase
+
+console.log(msg.toLowerCase()); // good morning
+
+// upperCase
+
+console.log(msg.toUpperCase());  //GOOD MORNING
+
+//substr
+console.log(msg.substr(0,4)); // good
+
+console.log(msg.substr(5));  //morning
+
+//charAt
+
+console.log(msg.charAt(5));  //M
+
+//charCodeAt
+
+console.log(msg.charCodeAt(5)); //77
